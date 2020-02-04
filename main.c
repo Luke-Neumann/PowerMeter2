d2006f4744f6cb250980ed2874891bfb69831311
Luke-Neumann <52217717+Luke-Neumann@users.noreply.github.com>
2020-02-03 22:49:52 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -650,13 +650,14 @@ int main(void)
     
 
     start_timer0();
+    global_open_start_gate = 1;
     
 
     /* insert your hardware initialization here */
     while(1){
         
         //global_open_start_gate = 1;
-        _delay_ms(5000);
+       // _delay_ms(5000);
         //generate_message();
     }
     return 0;
